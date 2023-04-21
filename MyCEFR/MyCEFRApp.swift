5aa39b8e201d902c7315c4f678c2d25b5b32fb92
deshabml <deshab@yandex.ru>
2023-04-21 20:23:23 +0300

    Initial Commit

---
+++ MyCEFR/MyCEFRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCEFRApp.swift
+//  MyCEFR
+//
+//  Created by Лаборатория on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCEFRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
