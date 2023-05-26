b0583414010d6f8096ae755634d418ff937c5cbe
deshabml <deshab@yandex.ru>
2023-05-26 16:00:02 +0300

    Add show registration

--- MyCEFR/View/AuthorizationView.swift
+++ MyCEFR/View/AuthorizationView.swift
@@ -10,21 +10,100 @@ import SwiftUI
 struct AuthorizationView: View {
 
     @StateObject var viewModel = AuthorizationViewModel()
+    @State var isAuthorization = true
+    @State var isShowCodeTextFild = false
 
     var body: some View {
         VStack {
-            Text("Авторизуйтесь")
-                .font(.custom("ItimCyrillic", size: 24))
-                .padding(.vertical, 50)
-            VStack {
-                TextField("Login", text: $viewModel.loginText)
-                    .padding()
-                    .background(.white)
-                SecureField("Password", text: $viewModel.loginText)
-                    .padding()
-                    .background(.white)
+
+            Text(isAuthorization ? "Авторизуйтесь" : "Зарегистрируйтесь")
+                .font(.custom("ItimCyrillic", size: 28))
+                .padding(.vertical, 30)
+            VStack(spacing: 10) {
+                if isShowCodeTextFild {
+                    ZStack {
+                        Text(viewModel.loginText)
+                            .font(.custom("ItimCyrillic", size: 18))
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundColor(.white)
+                            .background(.black.opacity(0.6))
+                        HStack {
+                            Spacer()
+                            Button {
+                                isShowCodeTextFild.toggle()
+                                viewModel.isShowButtonSendCode.toggle()
+                            } label: {
+                                Image(systemName: "square.and.pencil")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(Color("RedTopicColor"))
+                                    .padding(.horizontal, 8)
+                            }
+                        }
+                    }
+                    TextField("Введите код", text: $viewModel.verificationCodeText)
+                        .font(.custom("ItimCyrillic", size: 28))
+                        .frame(width: 100)
+                        .padding()
+                        .background(.white)
+                } else {
+                    TextField("E-mail", text: $viewModel.loginText)
+                        .font(.custom("ItimCyrillic", size: 18))
+                        .padding()
+                        .background(.white)
+                }
+                if viewModel.isShowButtonSendCode, !isAuthorization {
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.isShowButtonSendCode.toggle()
+                            isShowCodeTextFild.toggle()
+                        } label: {
+                            Text("Выслать код")
+                                .font(.custom("ItimCyrillic", size: 24))
+                                .foregroundColor(.black)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 10)
+                                .background(Color("MainTopicColor"))
+                        }
+                    }
+                    .padding(.horizontal, 2)
+                }
+                if isAuthorization {
+                    ZStack {
+                        SecureField("Password", text: $viewModel.passwordText)
+                            .font(.custom("ItimCyrillic", size: 18))
+                            .padding()
+                            .background(.white)
+                        if viewModel.isShowButtonLogIn {
+                            HStack {
+                                Spacer()
+                                Button {
+                                    print("log in")
+                                } label: {
+                                    Text("Войти")
+                                        .font(.custom("ItimCyrillic", size: 24))
+                                        .foregroundColor(.black)
+                                        .padding(.horizontal, 16)
+                                        .padding(.vertical, 10)
+                                        .background(Color("MainTopicColor"))
+                                }
+                            }
+                            .padding(.horizontal, 2)
+                        }
+                    }
+                }
+                Button {
+                    isAuthorization.toggle()
+                    isShowCodeTextFild = false
+                } label: {
+                    Text(isAuthorization ? "Ещё не с нами?" : "Уже есть аккаунт")
+                        .foregroundColor(Color("RedTopicColor"))
+                        .font(.custom("ItimCyrillic", size: 18))
+                }
             }
-            .padding(.vertical, 50)
+            .padding(.vertical, 70)
             Spacer()
         }
         .padding()
@@ -34,7 +113,12 @@ struct AuthorizationView: View {
              .resizable()
              .ignoresSafeArea()
              .scaledToFill()
+             .blur(radius: isAuthorization ? 0 : 12)
          )
+        .animation(.easeInOut(duration: 0.4), value: viewModel.isShowButtonLogIn)
+        .animation(.easeInOut(duration: 0.4), value: viewModel.isShowButtonSendCode)
+        .animation(.easeInOut(duration: 0.3), value: isAuthorization)
+        .animation(.easeInOut(duration: 0.3), value: isShowCodeTextFild)
     }
 }
 
--- MyCEFR/ViewModel/AuthorizationViewModel.swift
+++ MyCEFR/ViewModel/AuthorizationViewModel.swift
@@ -9,7 +9,35 @@ import Foundation
 
 class AuthorizationViewModel: ObservableObject {
 
-    @Published var loginText: String = ""
+    @Published var loginText: String = "" {
+        didSet {
+            toggleIsShowButtonLogIn()
+        }
+    }
+    @Published var passwordText: String = "" {
+        didSet {
+            toggleIsShowButtonLogIn()
+        }
+    }
+    @Published var verificationCodeText: String = ""
+    @Published var isShowButtonLogIn: Bool = false
+    @Published var isShowButtonSendCode: Bool = false
 
-    
+}
+
+extension AuthorizationViewModel {
+
+    func toggleIsShowButtonLogIn() {
+        if passwordText.count > 0, loginText.count > 0 {
+            isShowButtonLogIn = true
+        } else {
+            isShowButtonLogIn = false
+        }
+        if loginText.count > 0 {
+            isShowButtonSendCode = true
+        } else {
+            isShowButtonSendCode = false
+        }
+    }
+
 }
