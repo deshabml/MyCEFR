1334004cd693689eaced24111c1b265844aeab9a
deshabml <deshab@yandex.ru>
2023-09-16 00:23:59 +0300

    Put Authorization View in order

--- MyCEFR/View/AuthorizationView.swift
+++ MyCEFR/View/AuthorizationView.swift
@@ -16,113 +16,20 @@ struct AuthorizationView: View {
         VStack {
             VStack(spacing: 10) {
                 if viewModel.showCreatePassword, !viewModel.isAuthorization {
-                    VStack {
-                        SecureFieldView(viewModel: viewModel.createPasswordSFVMOne)
-                        SecureFieldView(viewModel: viewModel.createPasswordSFVMSecond)
-                    }
-                    if viewModel.showPasswordErrorText {
-                        Text(viewModel.passwordErrorText)
-                            .modifier(TextElement(size: 18,
-                                                  foregroundColor: .red))
-                    }
-                    if viewModel.showButtonCompleteRegistration {
-                            ButtonView(viewModel: viewModel.buttonRegComplitedViewModel,
-                                       color: (.black, Color("MainTopicColor")),
-                                       width: nil)
-                    }
+                    createPassword()
                 } else {
-                    if viewModel.showCodeTextFild {
-                        ZStack {
-                            Text(viewModel.loginTFVM.bindingProperty)
-                                .font(.custom("ItimCyrillic", size: 18))
-                                .padding()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.white)
-                                .background(.black.opacity(0.6))
-                                .cornerRadius(8)
-                            HStack {
-                                Spacer()
-                                ButtonImageView(viewModel: viewModel.buttomEditMailBIVM)
-                            }
-                        }
-                        TextFieldView(viewModel: viewModel.verificationCodeTFVM,
-                                      size: 24,
-                                      width: 110,
-                                      height: 24)
-                        if viewModel.showButtonSend {
-                            ButtonView(viewModel: viewModel.buttonSendViewModel,
-                                       color: (.black, Color("MainTopicColor")),
-                                       width: 110)
-                        }
-                    } else {
-                        TextFieldView(viewModel: viewModel.loginTFVM,
-                                      size: 18,
-                                      width: nil,
-                                      height: nil)
-                    }
+                    logInTextFild()
                 }
                 if viewModel.showButtonSendCode, !viewModel.isAuthorization {
-                    HStack {
-                        Spacer()
-                        ButtonView(viewModel: viewModel.buttonSendCodeViewModel,
-                                   color: (.black, Color("MainTopicColor")),
-                                           width: nil)
-                    }
-                    .padding(.horizontal, 2)
+                    buttonSendCode()
                 }
                 if viewModel.isAuthorization {
-                    VStack {
-                        ZStack {
-                            SecureFieldView(viewModel: viewModel.passwordSFVM)
-                            if viewModel.showButtonLogIn {
-                                HStack {
-                                    Spacer()
-                                    ButtonView(viewModel: viewModel.buttonLogInViewModel,
-                                               color: (.black, Color("MainTopicColor")),
-                                               width: nil)
-                                }
-                                .padding(.horizontal, 2)
-                            }
-                        }
-                        if viewModel.showlogInErrorText {
-                            Text(viewModel.logInErrorText)
-                                .modifier(TextElement(size: 18,
-                                                      foregroundColor: .red))
-                                .padding(10)
-                                .background(
-                                    Color("WhiteColor").blur(radius: 40)
-                                )
-                        }
-                    }
+                    passwordTextFild()
                 }
                 if viewModel.showlogInErrorText {
-                    HStack {
-                        Spacer()
-                        Button {
-                            viewModel.actionButtonAuthOrReg()
-                            viewModel.forgotPassword.toggle()
-                        } label: {
-                            Text("forgotPassword".localized)
-                                .modifier(TextElement(size: 18,
-                                                      foregroundColor: .white))
-                                .padding(10)
-                                .background(
-                                    Color("BlackColor").blur(radius: 50)
-                                )
-                        }
-                    }
+                    buttonForgotPassword()
                 }
-                Button {
-                    viewModel.actionButtonAuthOrReg()
-                } label: {
-                    Text(viewModel.buttonSwicthScreenText)
-                        .modifier(TextElement(size: 18,
-                                              foregroundColor: .white))
-                        .padding(10)
-                        .background(
-                            Color("BlackColor").blur(radius: 50)
-                        )
-                }
+                buttonSwicthScreenText()
             }
             .padding(.top, 170)
             Spacer()
@@ -156,6 +63,133 @@ struct AuthorizationView_Previews: PreviewProvider {
     static var previews: some View {
         AuthorizationView(viewModel: AuthorizationViewModel())
             .environmentObject(Coordinator(isWorker: false))
+    }
+
+}
+
+extension AuthorizationView {
+
+    private func createPassword() -> some View {
+        VStack {
+            SecureFieldView(viewModel: viewModel.createPasswordSFVMOne)
+            SecureFieldView(viewModel: viewModel.createPasswordSFVMSecond)
+            if viewModel.showPasswordErrorText {
+                Text(viewModel.passwordErrorText)
+                    .modifier(TextElement(size: 18,
+                                          foregroundColor: .red))
+            }
+            if viewModel.showButtonCompleteRegistration {
+                    ButtonView(viewModel: viewModel.buttonRegComplitedViewModel,
+                               color: (.black, Color("MainTopicColor")),
+                               width: nil)
+            }
+        }
+    }
+
+    private func logInTextFild() -> some View {
+        VStack {
+            if viewModel.showCodeTextFild {
+                ZStack {
+                    Text(viewModel.loginTFVM.bindingProperty)
+                        .font(.custom("ItimCyrillic", size: 18))
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.white)
+                        .background(.black.opacity(0.6))
+                        .cornerRadius(8)
+                    HStack {
+                        Spacer()
+                        ButtonImageView(viewModel: viewModel.buttomEditMailBIVM)
+                    }
+                }
+                TextFieldView(viewModel: viewModel.verificationCodeTFVM,
+                              size: 24,
+                              width: 110,
+                              height: 24)
+                if viewModel.showButtonSend {
+                    ButtonView(viewModel: viewModel.buttonSendViewModel,
+                               color: (.black, Color("MainTopicColor")),
+                               width: 110)
+                }
+            } else {
+                TextFieldView(viewModel: viewModel.loginTFVM,
+                              size: 18,
+                              width: nil,
+                              height: nil)
+            }
+        }
+    }
+
+    private func passwordTextFild() -> some View {
+        VStack {
+            ZStack {
+                SecureFieldView(viewModel: viewModel.passwordSFVM)
+                if viewModel.showButtonLogIn {
+                    buttonLogIn()
+                }
+            }
+            if viewModel.showlogInErrorText {
+                Text(viewModel.logInErrorText)
+                    .modifier(TextElement(size: 18,
+                                          foregroundColor: .red))
+                    .padding(10)
+                    .background(
+                        Color("WhiteColor").blur(radius: 40)
+                    )
+            }
+        }
+    }
+
+    private func buttonSendCode() -> some View {
+        HStack {
+            Spacer()
+            ButtonView(viewModel: viewModel.buttonSendCodeViewModel,
+                       color: (.black, Color("MainTopicColor")),
+                               width: nil)
+        }
+        .padding(.horizontal, 2)
+    }
+
+    private func buttonLogIn() -> some View {
+        HStack {
+            Spacer()
+            ButtonView(viewModel: viewModel.buttonLogInViewModel,
+                       color: (.black, Color("MainTopicColor")),
+                       width: nil)
+        }
+        .padding(.horizontal, 2)
+    }
+
+    private func buttonForgotPassword() -> some View {
+        HStack {
+            Spacer()
+            Button {
+                viewModel.actionButtonAuthOrReg()
+                viewModel.forgotPassword.toggle()
+            } label: {
+                Text("forgotPassword".localized)
+                    .modifier(TextElement(size: 18,
+                                          foregroundColor: .white))
+                    .padding(10)
+                    .background(
+                        Color("BlackColor").blur(radius: 50)
+                    )
+            }
+        }
+    }
+
+    private func buttonSwicthScreenText() -> some View {
+        Button {
+            viewModel.actionButtonAuthOrReg()
+        } label: {
+            Text(viewModel.buttonSwicthScreenText)
+                .modifier(TextElement(size: 18,
+                                      foregroundColor: .white))
+                .padding(10)
+                .background(
+                    Color("BlackColor").blur(radius: 50)
+                )
+        }
     }
 
 }
