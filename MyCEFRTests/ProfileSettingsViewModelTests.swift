67614d284197c7df625477a81efa035785412451
deshabml <deshab@yandex.ru>
2023-10-12 01:26:46 +0300

    Added testEditUserDataShouldIsShowToggle

--- MyCEFRTests/ProfileSettingsViewModelTests.swift
+++ MyCEFRTests/ProfileSettingsViewModelTests.swift
@@ -20,6 +20,35 @@ final class ProfileSettingsViewModelTests: XCTestCase {
         viewModel = nil
     }
 
+    func testSetupShouldSuccessFulNotSetup() {
+        guard let viewModel else {
+            XCTFail()
+            return
+        }
+        XCTAssertNil(viewModel.completeonUpdatingUser)
+        XCTAssertNil(viewModel.buttonExitVM.action)
+    }
 
+    func testSetupShouldSuccessFulSetup() {
+        guard let viewModel else {
+            XCTFail()
+            return
+        }
+        viewModel.setup {
+            let test = 4
+        }
+        XCTAssertNotNil(viewModel.completeonUpdatingUser)
+        XCTAssertNotNil(viewModel.buttonExitVM.action)
+    }
+
+    func testEditUserDataShouldIsShowToggle() {
+        guard let viewModel else {
+            XCTFail()
+            return
+        }
+        let isShow = viewModel.editPVM.showScreenEditProfile.isShow
+        viewModel.editUserData()
+        XCTAssertNotEqual(isShow, viewModel.editPVM.showScreenEditProfile.isShow)
+    }
 
 }
