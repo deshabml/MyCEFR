9bfd106a6bf122f461ab005d2d7ce0c6a97e4f80
deshabml <deshab@yandex.ru>
2023-11-10 23:05:37 +0300

    Added WordGroupScreen

---
+++ MyCEFR/View/WordGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  WordGroupView.swift
+//  MyCEFR
+//
+//  Created by Лаборатория on 10.11.2023.
+//
+
+import SwiftUI
+
+struct WordGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WordGroupView()
+}
