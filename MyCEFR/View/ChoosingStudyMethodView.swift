dc9701bcc27f3c5d70c40d0fa2e90e79103de8e0
deshabml <deshab@yandex.ru>
2023-12-01 12:10:35 +0300

    Added Buttons

--- MyCEFR/View/ChoosingStudyMethodView.swift
+++ MyCEFR/View/ChoosingStudyMethodView.swift
@@ -12,10 +12,70 @@ struct ChoosingStudyMethodView: View {
     @EnvironmentObject var coordinator: Coordinator
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            choosingStudyMethodButton("flashcards".localized,
+                                      "wordsReview".localized,
+                                      completion: {
+                print("Screen Show")
+            })
+            fakeButton("learn".localized,
+                       "learnWords".localized)
+            fakeButton("test".localized,
+                       "TakeATestToCheckYourKnowledge".localized)
+        }
+        .padding()
     }
 }
 
 #Preview {
     ChoosingStudyMethodView()
+}
+
+extension ChoosingStudyMethodView {
+
+    private func choosingStudyMethodButton(_ heading: String,_ explanation: String, completion: @escaping ()->()) -> some View {
+        Button {
+            completion()
+        } label: {
+            HStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(heading)
+                        .font(Font.custom("Spectral", size: 22)
+                            .weight(.semibold))
+                        .foregroundStyle(.white)
+                    Text(explanation)
+                        .font(Font.custom("Spectral", size: 16)
+                            .weight(.semibold))
+                        .foregroundStyle(Color(red: 0.59, green: 0.59, blue: 0.59))
+                }
+                .padding(.horizontal)
+                Spacer()
+            }
+        }
+        .frame(width: 300, height: 70)
+        .background(Color("MainBlueColor"))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .padding(.bottom, 16)
+    }
+
+    private func fakeButton(_ heading: String,_ explanation: String) -> some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text(heading)
+                    .font(Font.custom("Spectral", size: 22)
+                        .weight(.semibold))
+                    .foregroundStyle(.white)
+                Text(explanation)
+                    .font(Font.custom("Spectral", size: 16)
+                        .weight(.semibold))
+                    .foregroundStyle(Color(red: 0.59, green: 0.59, blue: 0.59))
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
+        .frame(width: 300, height: 70)
+        .background(Color("MainBlueColor").opacity(0.4))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .padding(.bottom, 16)
+    }
 }
